e1b613c14a2653320badd9083e9afac3530d481a
Gautam <120419555+gautam10474609@users.noreply.github.com>
2022-12-16 20:53:13 -0500

    added contactus and comments

--- data/comments.js
+++ data/comments.js
@@ -3,6 +3,7 @@ const { ObjectId } = require('mongodb');
 const mongoCollections = require("../config/mongoCollections");
 const comments = mongoCollections.comments;
 const reviews = mongoCollections.reviews;
+<<<<<<< Updated upstream
 const students = mongoCollections.students;
 // const uuid = require('uuid/v4');
 
@@ -12,12 +13,23 @@ module.exports = {
         if (!studentId || (typeof studentId != "string")) throw "studentId must be given as a string";
         if (!reviewId || (typeof reviewId != "string")) throw "reviewId must be given as a string";
         if (!commentText || (typeof commentText != "string")) throw "must give comment text as a string";
+=======
+const validate = require('../helper');
+const { ObjectId } = require('mongodb');
+
+module.exports = {
+    async createComment(studentId, reviewId, commentInput) {
+        id = await validate.validateId("createComment", studentId, "studentId");
+        id = await validate.validateId("createComment", reviewId, "validateId");
+        commentInput = await validate.validateString("createComment", commentInput, "commentInput")
+>>>>>>> Stashed changes
         const commentCollection = await comments();
         let newComment = {
             studentId: studentId,
             reviewId: reviewId,
             commentText: commentText
         }
+<<<<<<< Updated upstream
         const insertInfo = await commentCollection.insertOne(newComment);
         
         const revCollection = await reviews();
@@ -32,6 +44,23 @@ module.exports = {
             const updatedInfo = await revCollection.updateOne({ _id: objIdForRev }, { $push: { comments: String(newComment._id) } });
             if (updatedInfo.modifiedCount === 0) {
                 throw 'Could not update Review Collection with Review Data!';
+=======
+        const insertInfo = await commentCollection.insertOne(addNewComment);
+        const reviewCollection = await reviews();
+        if (!insertInfo.acknowledged || !insertInfo.insertedId) {
+            throw 'createComment: Could not add new Review';
+        } else {
+            const updatedInfo = await reviewCollection.updateOne({
+                _id: ObjectId(reviewId)
+            }, {
+                $push:
+                {
+                    comments: addNewComment._id.toString()
+                }
+            });
+            if (!updatedInfo.matchedCount && !updatedInfo.modifiedCount) {
+                throw 'createComment: Could not update Review Collection with Review Data!';
+>>>>>>> Stashed changes
             }
             //Add the comment id to the user
             // const updatedInfo2 = await studentsCollection.updateOne({ _id: objIdForStudents }, { $push: { commentIds: String(newComment._id) } });
@@ -39,6 +68,7 @@ module.exports = {
             //     throw 'Could not update Studentss Collection with Review Data!';
             // }
         }
+<<<<<<< Updated upstream
 
         const newId = insertInfo.insertedId;
         const newIDString = String(newId);
@@ -52,6 +82,24 @@ module.exports = {
         const commentCollection = await comments();
         const comment = await commentCollection.findOne({ _id: id});
         if (!comment) throw "Comment with that id does not exist";
+=======
+        const commentId = insertInfo.insertedId.toString();
+        return await this.getComment(commentId);
+    },
+
+    async getAllComments() {
+        const commentCollection = await comments();
+        const allComment = await commentCollection.find({}).toArray();
+        if (!allComment) throw "getAllComments: No comments available";
+        return allComment;
+    },
+
+    async getComment(id) {
+        id = await validate.validateId("getComment", id, "commentId");
+        const commentCollection = await comments();
+        const comment = await commentCollection.findOne({ _id: ObjectId(id) });
+        if (!comment) throw `getComment: No comment exists with the ${id}`;
+>>>>>>> Stashed changes
         return comment;
     },
 
@@ -69,6 +117,7 @@ module.exports = {
         let comment = await this.getComment(id);
         const deleteInfo = await commentCollection.removeOne({ _id: id});
         if (deleteInfo.deletedCount === 0) {
+<<<<<<< Updated upstream
             throw "could not delete comment with id of ${id}";
         }
         try {
@@ -82,12 +131,31 @@ module.exports = {
             }
         } catch (e) {
             throw "Could not Delete Comment from Review while Deleting Comment!";
+=======
+            throw `removeComment: No comment exists with the ${id}`;
+        }
+        try {
+            const reviewCollection = await reviews();
+            const deletionInfoForCommentFromReview = await reviewCollection.updateOne({
+                _id: ObjectId(comment.reviewId)
+            }, {
+                $pull: {
+                    comments: id.toString()
+                }
+            });
+            if (deletionInfoForCommentFromReview.deletedCount === 0) {
+                throw `removeComment: Could not delete Comment ${id}`;
+            }
+        } catch (error) {
+            throw "removeComment: Not able delete comment from review comments";
+>>>>>>> Stashed changes
         }
 
         return true;
     },
     
     async updateComment(id, commentText) {
+<<<<<<< Updated upstream
         if (!id) throw "Comment Id is missing";
         
         const updatedCommentData = {};
@@ -101,6 +169,20 @@ module.exports = {
         const commentCollection = await comments();
         const updateCommentInfo = await commentCollection.updateOne({ _id: id }, { $set: updatedCommentData });
         if (updateCommentInfo.modifiedCount === 0) throw "Could not update comment";
+=======
+        id = await validate.validateId("updateComment", id, "commentId");
+        commentText = await validate.validateString("updateComment", commentText, "commentInput")
+        const updateCommentData = {};
+        updateCommentData.commentText = commentText;
+        const commentCollection = await comments();
+        const updateCommentInfo = await commentCollection.updateOne({
+            _id: ObjectId(id)
+        },
+            {
+                $set: updateCommentData
+            });
+        if (!updateCommentInfo.modifiedCount) throw "updateComment: Could not able update Comment";
+>>>>>>> Stashed changes
         return await this.getComment(id);
     }
 }
--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const data = require('../data/');
 const comments = data.comments;
 
+<<<<<<< Updated upstream
 router.get("/:id", async (req, res) => {
     try {
       const comment = await comments.getComment(req.params.id);
@@ -76,6 +77,108 @@ router.post("/:courseId/:commentId/edit", async (req, res) => {
   } catch (e) {
     console.log(e);
     res.status(404).json ({message: "Could not update comment!"});
+=======
+router.get('/:id', async (req, res) => {
+  try {
+    return res.redirect("/courses");
+  } catch (error) {
+    return res.status(404).render("error",{error: error});
+  }
+});
+
+router.post('/:studentId/:reviewId/:courseId/add', async (req, res) => {
+  let reviewId = req.params.reviewId
+  let studentId = req.params.studentId
+  let courseId = req.params.courseId
+  let commentVal = req.body.commentValue
+  try {
+    reviewId = await validate.validateId("Post Add", reviewId, "Review Id");
+  } catch (error) {
+    return res.status(400).render("error",{error: error});
+  }
+  try {
+    studentId = await validate.validateId("Post Add", studentId, "Student Id");
+  } catch (error) {
+    return res.status(400).render("error",{error: error});
+  }
+  try {
+    courseId = await validate.validateId("Post Add", courseId, "Course Id");
+  } catch (error) {
+    return res.status(400).render("error",{error: error});
+  }
+  try {
+    commentVal = await validate.validateString("Post Add", commentVal, "Comment");
+  } catch (error) {
+    return res.status(400).render("error",{error: error});
+  }
+  try {
+    coomentData = await comments.createComment("Post Add", studentId, reviewId, commentVal)
+    if (coomentData) return res.redirect("/courses/" + courseId);
+    else return res.status(404).send();
+  } catch (error) {
+    return res.status(500).render("error",{error: error});
+  }
+});
+
+router.get('/:courseId/:commentId/delete', async (req, res) => {
+  let courseId = req.params.courseId
+  let commentId = req.params.commentId
+  try {
+    courseId = await validate.validateId("Get delete", courseId, "Course Id");
+  } catch (error) {
+    return res.status(400).render("error",{error: error});
+    
+  }
+
+  try {
+    commentId = await validate.validateId("Get delete", commentId, "Comment Id");
+  } catch (error) {
+    return res.status(400).render("error",{error: error});
+  }
+  try {
+    await comments.getComment(commentId);
+  } catch (error) {
+    return res.status(404).render("error",{error: error});
+  }
+  try {
+    delComm = await comments.removeComment(commentId);
+    if (delComm) {
+      return res.redirect("/courses/" + courseId);
+    } else {
+      return res.status(404).send();
+    }
+  } catch (error) {
+    return res.status(500).render("error",{error: error});
+  }
+});
+
+router.post("/:courseId/:commentId/edit", async (req, res) => {
+  let courseId = req.params.courseId
+  let commentId = req.params.commentId
+  let commentVal = req.body.commentValue;
+  try {
+    courseId = await validate.validateId("Post Edit", courseId, "Course Id");
+  } catch (error) {
+    return res.status(400).render("error",{error: error});
+  }
+
+  try {
+    commentId = await validate.validateId("Post Edit", commentId, "Comment Id");
+  } catch (error) {
+    return res.status(400).render("error",{error: error});
+  }
+
+  try {
+    commentVal = await validate.validateId("Post Edit", commentVal, "Comment");
+  } catch (error) {
+    return res.status(400).render("error",{error: error});
+  }
+  try {
+    await comments.updateComment("Post Edit", commentId, commentVal);
+    return res.redirect("/courses/" + courseId);
+  } catch (error) {
+    return res.status(404).render("error",{error: error});
+>>>>>>> Stashed changes
   }
 });
 

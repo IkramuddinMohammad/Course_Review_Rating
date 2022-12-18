28da508c53675fdfabe9a1d66800035169fcb703
ikrahim <100006054+IkramuddinMohammad@users.noreply.github.com>
2022-12-18 18:17:49 -0500

    fixing the add review for the student logged in

--- routes/courses.js
+++ routes/courses.js
@@ -453,8 +453,10 @@ router.get("/:id", async (req, res) => {
           if(studentData.coursesList){
             let getStuCourseList = studentData.coursesList
             for(let i=0; i<getStuCourseList.length; i++){
+              console.log("test",course.courseId.toString(), getStuCourseList[i].toString())
               if(course.courseId.toString() === getStuCourseList[i].toString()){
                 studentData.reviewbtnEnable =true
+                break 
               }else{
                 studentData.reviewbtnEnable =false
               }
